8b42498bbf516a0a8ef23c6b5b2d6cef246a4067
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-20 10:28:23 -0300

    Update StartApp.java

--- src/main/java/com/example/demo/StartApp.java
+++ src/main/java/com/example/demo/StartApp.java
@@ -1,23 +1,21 @@
 package com.example.demo;
-
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 @Component
 public class StartApp implements CommandLineRunner {
-    @Autowired
-    private UserRepository repository;
-    @Override
-    public void run(String... args) throws Exception {
-        User user = new User();
-        user.setUsername("moura_jg");
-        user.setPassword("12345678");
-        repository.save(user);
-        for (User u: repository.findAll()){
-            System.out.println(u);
-        }
-    }
+	@Autowired
+	private UserRepository repository;
+	@Override
+	public void run (String... args) throws Exception {
+		User user = new User ();
+		user.setUsername ("moura_jg");
+		user.setPassword ("12345678");
+		repository.save (user);
+		for (User u: repository.findAll ()) {
+			System.out.println (u);
+		}
+	}
 }
