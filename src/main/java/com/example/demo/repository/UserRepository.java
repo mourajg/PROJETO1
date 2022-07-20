be46106025d3693dd9977528c777d3ab5d7fa21c
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-20 10:29:43 -0300

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository <User, Integer> {
 }
