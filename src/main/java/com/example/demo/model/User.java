85921c2c35b71cf75e12886404aae1610a9c5d41
João <contatomoura2000@gmail.com>
2022-07-20 10:14:18 -0300

    1

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(length = 8, nullable = false)
+    private String username;
+    @Column(length = 8, nullable = false)
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
